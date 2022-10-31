5354e8b37f205b3e51c74f3c8d9fbbc5f84f9cdf
EstebanMarin <estebanmarin002@gmail.com>
2022-10-31 17:21:57 -0500

    Decoding Failures

--- circe-scalafx/src/main/scala/circefx/Circefx.scala
+++ circe-scalafx/src/main/scala/circefx/Circefx.scala
@@ -1,5 +1,23 @@
 package circefx
 
+import fx.*
+import fx.Control
 import io.circe.*
+import cats.*
+import cats.data.*
+import cats.implicits.*
+
+object implicits:
+  extension [A](fa: ValidatedNel[DecodingFailure, A])
+    def bind(using fx.Control[NonEmptyList[DecodingFailure]]): A =
+      fa.fold(_.shift, identity)
 
 class Circefx
+
+def runProgram =
+//   def program[A](c: Json)(using d: Decoder[A], cx: Control[DecodingFailure]): A =
+  def program[A](c: io.circe.HCursor)(using d: Decoder[A], cx: Control[DecodingFailure]): A =
+    d(c).bind
+
+  // Parsed document
+  val test: DecodingFailure | String = run(program[String](???))
