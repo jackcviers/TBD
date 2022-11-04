6c4eacbc19735e8b7a9eac3032f8b77252206e5c
EstebanMarin <estebanmarin002@gmail.com>
2022-11-04 14:52:06 -0500

    updating transform

--- circe-scalafx/src/main/scala/circefx/Circefx.scala
+++ circe-scalafx/src/main/scala/circefx/Circefx.scala
@@ -10,14 +10,17 @@ import cats.implicits.*
 class Circefx
 
 def runProgram =
+  // TODO test with a Json
   def decodeTest[A](c: io.circe.HCursor)(using d: Decoder[A], cx: Control[DecodingFailure]): A =
     d(c).bind
 
   def parserTest[A](s: String)(using p: Parser, cx: Control[Error], d: Decoder[A]) =
     p.decode(s).bind
 
-    //TODO
-  // def parserTest2[A](s: String)(using p: Parser, cx: Control[Error], d: Decoder[A]) =
-  //   p.decodeAccumulating(s).bind
+  def transform[E, A](s: ValidatedNel[E, A]): Either[E, A] =
+    s.fold((e: NonEmptyList[E]) => Left(e.fold(???)), (a: A) => Right(a))
+
+  def parserTest2[A](s: String)(using p: Parser, cx: Control[Error], d: Decoder[A]) =
+    transform(p.decodeAccumulating(s)).bind
 
   val test: DecodingFailure | String = run(decodeTest[String](???))
